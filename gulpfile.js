37c11f4eef34cafe96a794494aa899d8f2d6237b
Steven <info@truediggs.com>
2016-07-27 19:29:03 -0400

    add uglify

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,44 @@ var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
 	browserify = require('gulp-browserify'),
-	concat = require('gulp-concat');
 	compass = require('gulp-compass'),
-	connect = require('gulp-connect');
-	
+	connect = require('gulp-connect'),
+	gulpif = require('gulp-if'),
+	uglify = require('gulp-uglify'),
+	concat = require('gulp-concat');
 
-var coffeeSources = ['components/coffee/tagline.coffee'];
-var jsSources = [
+var env, 
+	coffeeSources,  
+	jsSources, 
+	sassSources, 
+	htmlSources, 
+	jsonSources, 
+	outputDir,
+	sassStyle;
+
+env = process.env.NODE_ENV || 'development';
+
+if (env ==='development') {
+	outputDir = 'builds/development/';
+	sassStyle = 'expanded';
+}
+
+else {
+	outputDir = 'builds/production/';
+	sassStyle = 'compressed';
+}
+
+coffeeSources = ['components/coffee/tagline.coffee'];
+
+jsSources = [
 'components/scripts/rclick.js',
 'components/scripts/pixgrid.js',
 'components/scripts/tagline.js',
 'components/scripts/template.js'];
 
-var sassSources = ['components/sass/style.scss'];
-var htmlSources = ['builds/development/*.html'];
-var jsonSources = ['builds/development/js/*.json'];
+sassSources = ['components/sass/style.scss'];
+htmlSources = [outputDir + '*.html'];
+jsonSources = [outputDir + 'js/*.json'];
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
@@ -30,7 +53,8 @@ gulp.task('js', function() {
 	gulp.src(jsSources)
 		.pipe(concat('script.js'))
 		.pipe(browserify())
-		.pipe(gulp.dest('builds/development/js'))
+		.pipe(gulpif(env === 'production', uglify()))
+		.pipe(gulp.dest(outputDir +'js'))
 		.pipe(connect.reload())
 });
 
@@ -39,11 +63,11 @@ gulp.task('compass', function() {
 	gulp.src(sassSources)
 		.pipe(compass({
 			sass: 'components/sass',
-			image: 'builds/development/images',
-			style: 'expanded'
+			image: outputDir + '/images',
+			style: sassStyle
 		}))
 		.on('error', gutil.log)
-		.pipe(gulp.dest('builds/development/css'))
+		.pipe(gulp.dest(outputDir + 'css'))
 		.pipe(connect.reload())
 
 });
@@ -60,7 +84,7 @@ gulp.task('watch', function() {
 
 gulp.task('connect', function () {
 	connect.server({
-		root: 'builds/development/',
+		root: outputDir,
 		livereload: true,
 		port: 8080
 	});
@@ -73,7 +97,7 @@ gulp.task('html', function () {
 });
 
 gulp.task('json', function () {
-	gulp.src('builds/development/js/*.json')
+	gulp.src(outputDir + 'js/*.json')
 	.pipe(connect.reload())
 });
 
